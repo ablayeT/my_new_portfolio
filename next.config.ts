cff8e44f5761671576c535dfacb63f219bef33a6
abdoulaye toure <ablayetoure2014@gmail.com>
2025-09-26 19:16:42 +0200

    update deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  experimental: {
-    turbopack: { root: "." },
-    esModuleInterop: true,
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  // En prod, on ne bloque pas le build à cause d'ESLint
+  eslint: {
+    ignoreDuringBuilds: true,
   },
+
+  // TypeScript bloquer si erreurs de typage (true pour débloquer temporairement)
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+
+  // Optionnel (utile pour déploiement Node sur VPS)
+  // output: "standalone",
 };
-module.exports = nextConfig;
+
+export default nextConfig;
